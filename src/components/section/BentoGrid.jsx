b27ca03e98e48b4d435fbbd7ba67c43c8be9f136
Ahmad Hasan <aaahmaaad.hsn@gmail.com>
2025-04-28 23:01:36 +0700

    add tombol see more

--- src/components/section/BentoGrid.jsx
+++ src/components/section/BentoGrid.jsx
@@ -9,6 +9,7 @@ import {
   IconSignature,
   IconTableColumn,
 } from "@tabler/icons-react";
+import { motion } from "motion/react";
 
 export function BentoGridDemo() {
   return (
@@ -23,6 +24,22 @@ export function BentoGridDemo() {
           className={i === 3 || i === 6 ? "md:col-span-2" : ""}
         />
       ))}
+      <motion.a
+        whileHover={{ scale: 1.1 }}
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br cursor-pointer from-slate-300 to-slate-500 py-2 bg-clip-text text-center text-xl font-medium tracking-tight  md:text-xl sm:text-sm border-2 border-slate-300 rounded-4xl px-4  text-white hover:bg-slate-300 hover:text-blue-950 hover:border-blue-900 transition duration-300 ease-in-out"
+        // onClick={() => Navigate("/booking")}
+        href="/show"
+      >
+        Get ticket NOW
+      </motion.a>
+
     </BentoGrid>
   );
 }
